f59bf26e22dc56d93d19b0ba0ef2eb5e47812258
Nizar <ntahri@student.42.fr>
2025-10-19 02:46:27 +0200

    correction exit

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,34 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ntahri <ntahri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 04:06:14 by ntahri            #+#    #+#             */
-/*   Updated: 2025/10/18 00:30:43 by jegerman         ###   ########.fr       */
+/*   Updated: 2025/10/19 02:40:34 by ntahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// conversion sécurisée de chaîne en long avec détection des erreurs
 static int	ft_atol_safe(const char *str, long *res)
 {
 	int				sign;
 	unsigned long	val;
-	int				digit;
 
-	val = 0;
-	sign = 1;
 	if (!str || !*str)
 		return (0);
-	if ((*str == '+' || *str == '-') && *str++ == '-')
-		sign = -1;
+	sign = 1;
+	val = 0;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
 	while (*str)
 	{
 		if (!ft_isdigit(*str))
 			return (0);
-		digit = *str - '0';
-		val = val * 10 + digit;
+		val = val * 10 + (*str - '0');
+		if (val > LONG_MAX
+			&& !(sign == -1 && val == (unsigned long)LONG_MAX + 1))
+			return (0);
 		str++;
 	}
 	*res = (long)(val * sign);
@@ -41,13 +45,13 @@ int	bi_exit(t_core *core, t_cmd *cmd)
 {
 	long	code;
 
-	ft_putendl_fd("exit", STDERR_FILENO);
+	ft_putendl_fd("exit", STDOUT_FILENO);
 	if (cmd->argc < 2)
 	{
 		utl_cleanup(core->flags | FLG_ENV, core);
 		exit(core->exit);
 	}
-	if (cmd->argc > 2) // 18/10 - argc == 2? Potential bug out there.
+	if (cmd->argc > 2)
 	{
 		ft_eprintf(ERR_BMNY, "exit");
 		return (core->exit = 1);
@@ -55,8 +59,9 @@ int	bi_exit(t_core *core, t_cmd *cmd)
 	if (!ft_atol_safe(cmd->argv[1], &code))
 	{
 		ft_eprintf(ERR_BINV, "exit", cmd->argv[1]);
-		code = 2;
+		utl_exit(2, core);
 	}
 	utl_cleanup(core->flags | FLG_ENV, core);
-	exit(core->exit = code % 255);
+	exit(core->exit = (unsigned char)code);
 }
+
